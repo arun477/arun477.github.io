d18cf5829f55964a34980c64a75c5140ed3fed5b
carlfeynman <coolarun477@gmail.com>
2025-03-23 12:55:06 +0530

    Source update Sun Mar 23 12:55:06 IST 2025

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,8 @@ git add .
 git commit -m "Source update $(date)"
 git push origin main
 
-# Clean the public directory
-rm -rf public
-
 # Build the Hugo site with the production baseURL
-hugo --gc --minify --baseURL="https://arun477.github.io/" 
+hugo --baseURL="https://arun477.github.io/" 
 
 # Navigate to the public directory
 cd public
